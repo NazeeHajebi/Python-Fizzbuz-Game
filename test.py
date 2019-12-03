a87306edc1eea075002cadbe3b08ba474fbdda5f
Nazee <none@right.now>
2019-12-02 20:45:04 -0800

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,15 @@ import pytest
 
 
 def fizz_buzz(number):
+    if number %3 == 0:
+        return "fizz"
     return str(number)
 
 
 def test_answer():
     check(1, "1")
     check(2, "2")
+    check(3, "fizz")
 
 
 def check(number, expected_result):
